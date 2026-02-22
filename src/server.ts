7598822c82ddd7ebfe5118c46df6b622ae58787b
loki0b <contato@henriquegarcialeite.com>
2026-02-22 14:03:31 -0300

    (feat): add card and collections routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import cookieParser from "cookie-parser";
 import userRoutes from "@routes/userRoutes"
+import cardRoutes from "@routes/cardRoutes";
+import collectionRoutes from "@routes/collectionRoutes";
 
 import type { Express } from "express";
 
@@ -11,6 +13,8 @@ const port: string | number = process.env["PORT"] || 8080;
 app.use(json());
 app.use(cookieParser());
 app.use("/api/user/", userRoutes);
+app.use("/api/cards", cardRoutes);
+app.use("api/collections", collectionRoutes);
 
 app.listen(port, () => {
     console.log(`Service running: http://localhost:${port}/`)
