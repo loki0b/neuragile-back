5b5bafbf124a5658ab7f2090a9412cc3619be61f
loki0b <contato@henriquegarcialeite.com>
2026-03-09 15:41:29 -0300

    (feat): add cors package

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import cardRoutes from "./routes/cardRoutes.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
@@ -9,7 +10,12 @@ import type { Express } from "express";
 const app: Express = express();
 const port: string | number = process.env["PORT"] || 8080;
 
+const corsOptions = {
+  origin: "https://neuragile.vercel.app",
+  methods: ['GET', 'POST'],
+};
 
+app.use(cors(corsOptions));
 app.use(json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
