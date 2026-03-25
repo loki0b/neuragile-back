738163614e41cf1c58ba5e34db6c05a8e1e43ed5
loki0b <contato@henriquegarcialeite.com>
2026-03-25 13:04:46 -0300

    (feat): add conditional to suport dev mode

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import cardRoutes from "./routes/cardRoutes.js";
 import collectionRoutes from "./routes/collectionRoutes.js";
+import chatRoutes from "src/routes/chatRoutes.js"
 
 import type { Express } from "express";
 
@@ -21,5 +22,12 @@ app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/cards", cardRoutes);
 app.use("/api/collections", collectionRoutes);
+app.use("/api/chat", chatRoutes);
+
+if (process.env["NODE_ENV"] === 'development') {
+  app.listen(port, () => {
+    console.log(`Service running in: http://localhost:${port}`)
+  });
+}
 
 export default app;
